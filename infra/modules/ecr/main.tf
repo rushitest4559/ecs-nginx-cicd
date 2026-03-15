29d7f6e2e248a61232b1ba350a9fa4ffad9a92ff
rushi <rushitest4559@gmail.com>
2026-03-15 22:09:40 +0530

    ecr force delete

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,9 +1,10 @@
 resource "aws_ecr_repository" "this" {
-  name = "nginx-repo"
+  name                 = "nginx-repo"
   image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
+  force_delete = true
   tags = {
     Name = "nginx-ecr-repo"
   }
@@ -13,16 +14,16 @@ resource "aws_ecr_lifecycle_policy" "this" {
   repository = aws_ecr_repository.this.id
   policy = jsonencode({
     rules = [{
-        rulePriority = 1
-        description = "keep last 10 images"
-        selection = {
-            tagStatus = "any"
-            countType = "imageCountMoreThan"
-            countNumber = 10
-        }
-        action = {
-            type = "expire"
-        }
+      rulePriority = 1
+      description  = "keep last 10 images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 10
+      }
+      action = {
+        type = "expire"
+      }
     }]
   })
 }
